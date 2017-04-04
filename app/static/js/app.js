576e563ec20196653fbc8f9b7f03626a0611c2f2
leonnash21 <leonnash2008@gmail.com>
2017-04-04 06:23:50 +0000

    modifications

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,7 +1,2 @@
 // Your JavaScript Code here
-var app = angular.module('AngApp', []);
-app.controller('MainCtrl', function($scope, $http){
-	$http.get("/api/thumbnails").then(function(response){
-		$scope.thumbnails = response.data.thumbnails;
-	});
-})
+
